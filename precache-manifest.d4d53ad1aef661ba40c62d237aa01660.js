15a076b9f9699c7a44689c19862cab9260645091
Peter Popluhar <peter.popluhar@socialbakers.com>
2019-05-15 09:27:52 +0200

    Updates

---
+++ precache-manifest.d4d53ad1aef661ba40c62d237aa01660.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06d6e3f9e49dc4ff426a78635cf083d5",
+    "url": "/kill-bills-v1/index.html"
+  },
+  {
+    "revision": "fcb87b805aa8ec950b81",
+    "url": "/kill-bills-v1/static/css/main.795991f2.chunk.css"
+  },
+  {
+    "revision": "0723cb573b9308de64e4",
+    "url": "/kill-bills-v1/static/js/2.c9cb641a.chunk.js"
+  },
+  {
+    "revision": "fcb87b805aa8ec950b81",
+    "url": "/kill-bills-v1/static/js/main.4790b62d.chunk.js"
+  },
+  {
+    "revision": "b2e80027b8fa63c688d9",
+    "url": "/kill-bills-v1/static/js/runtime~main.e3298fb4.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/kill-bills-v1/precache-manifest.b30bf9ded7bb17095755ccd22f3b1628.js"
+  "/kill-bills-v1/precache-manifest.d4d53ad1aef661ba40c62d237aa01660.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/kill-bills-v1/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/kill-bills-v1/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
