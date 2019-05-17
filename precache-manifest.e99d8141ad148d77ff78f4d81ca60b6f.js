b03b240b3e8be93fd5a349c6eaaaa43d26ea8421
Peter Popluhar <popluhar.peter@gmail.com>
2019-05-17 08:15:39 +0200

    Updates

---
+++ precache-manifest.e99d8141ad148d77ff78f4d81ca60b6f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "848da550c4ded63a352d",
+    "url": "/kill-bills/static/js/runtime~main.9edc7381.js"
+  },
+  {
+    "revision": "2009a155a54976f9ad79",
+    "url": "/kill-bills/static/js/main.e43d92d5.chunk.js"
+  },
+  {
+    "revision": "ce1ccfc39b6ae68bcb08",
+    "url": "/kill-bills/static/js/2.08293375.chunk.js"
+  },
+  {
+    "revision": "2009a155a54976f9ad79",
+    "url": "/kill-bills/static/css/main.31f18c01.chunk.css"
+  },
+  {
+    "revision": "7e8b37f1519464a681e69a263791ab00",
+    "url": "/kill-bills/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -11,19 +11,13 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/kill-bills-v1/precache-manifest.4dcc302a4b8b97e133f476cc117774af.js"
+  "/kill-bills/precache-manifest.e99d8141ad148d77ff78f4d81ca60b6f.js"
 );
 
-self.addEventListener('message', (event) => {
-  if (event.data && event.data.type === 'SKIP_WAITING') {
-    self.skipWaiting();
-  }
-});
-
-workbox.core.clientsClaim();
+workbox.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -31,9 +25,10 @@ workbox.core.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/kill-bills-v1/index.html"), {
+workbox.routing.registerNavigationRoute("/kill-bills/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
